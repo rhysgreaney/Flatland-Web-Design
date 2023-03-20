2a61606d2078cc7fa629b6e4d2bd13d6eda6e653
rhysgreaney <126647880+rhysgreaney@users.noreply.github.com>
2023-03-20 15:28:08 +0800

    var words test

--- script.js
+++ script.js
@@ -10,6 +10,13 @@ square.addEventListener('click', (event) => changeColour ('green'));
 square.addEventListener('mouseover', (event) => changeColour ('grey'));
 square.addEventListener('mouseout', (event) => changeColour ('red'));
 
+var words = document.getElementById('words');
+function greeting() {
+    words.innerHTML = "Welcome to Flatland <br> I am Square!"
+}
+
+document.onload = greeting()
+
 function createBuzzwordPhrase() {
     /* See https://en.wikipedia.org/wiki/List_of_buzzwords */
     let buzz = ["Paradigm-changing", "Multi-tier", "10,000-foot", "Agile", "Customer", "Win-win"];
